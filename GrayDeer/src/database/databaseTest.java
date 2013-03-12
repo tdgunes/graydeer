b6a039c2e4a56550cab416b76cedb54c4f536db6
Eren Sezener <erensezener@gmail.com>
2013-03-12 09:42:46 +0200

    Add databasetest

---
+++ GrayDeer/src/database/databaseTest.java
@@ -0,0 +1,110 @@
+package database;
+
+/* Can Eren Sezener S003777 Department of Computer Science */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSetMetaData;
+
+
+public class databaseTest
+{
+    private static String dbURL = "jdbc:derby://localhost:1527/myDB;create=true;user=me;password=mine";
+    private static String tableName = "restaurants";
+    // jdbc Connection
+    private static Connection conn = null;
+    private static Statement stmt = null;
+
+    public static void main(String[] args)
+    {
+        createConnection();
+        insertRestaurants(5, "LaVals", "Berkeley");
+        selectRestaurants();
+        shutdown();
+    }
+    
+    private static void createConnection()
+    {
+        try
+        {
+            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL); 
+        }
+        catch (Exception except)
+        {
+            except.printStackTrace();
+        }
+    }
+    
+    private static void insertRestaurants(int id, String restName, String cityName)
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            stmt.execute("insert into " + tableName + " values (" +
+                    id + ",'" + restName + "','" + cityName +"')");
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    }
+    
+    private static void selectRestaurants()
+    {
+        try
+        {
+            stmt = conn.createStatement();
+            ResultSet results = stmt.executeQuery("select * from " + tableName);
+            ResultSetMetaData rsmd = results.getMetaData();
+            int numberCols = rsmd.getColumnCount();
+            for (int i=1; i<=numberCols; i++)
+            {
+                //print Column Names
+                System.out.print(rsmd.getColumnLabel(i)+"\t\t");  
+            }
+
+            System.out.println("\n-------------------------------------------------");
+
+            while(results.next())
+            {
+                int id = results.getInt(1);
+                String restName = results.getString(2);
+                String cityName = results.getString(3);
+                System.out.println(id + "\t\t" + restName + "\t\t" + cityName);
+            }
+            results.close();
+            stmt.close();
+        }
+        catch (SQLException sqlExcept)
+        {
+            sqlExcept.printStackTrace();
+        }
+    }
+    
+    private static void shutdown()
+    {
+        try
+        {
+            if (stmt != null)
+            {
+                stmt.close();
+            }
+            if (conn != null)
+            {
+                DriverManager.getConnection(dbURL + ";shutdown=true");
+                conn.close();
+            }           
+        }
+        catch (SQLException sqlExcept)
+        {
+            
+        }
+
+    }
+}
