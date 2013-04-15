d3656d6f22a26dc321f84fa1b6728cd5aa45fbd8
Taha Doğan Güneş <tdgunes@gmail.com>
2013-04-15 09:32:41 +0300

    reimplements StudentDB.java, although there is an implemented one
    And should never happen again :)

--- GrayDeer/src/server/student/StudentDB.java
+++ GrayDeer/src/server/student/StudentDB.java
@@ -4,6 +4,13 @@
  */
 package server.student;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import server.Utils;
 
 /**
@@ -19,10 +26,97 @@ public class StudentDB {
     private String dbFilePath = "";
     //this will be initialized
     
+    
+    private static ObjectInputStream input;
+    private static ObjectOutputStream output;
+    
     public StudentDB(String workdir){
         this.workDir = workdir;
         this.dbFilePath = Utils.combine(workdir, this.dbName);
         
     }
     
+    public ArrayList<Student> getStudents() {
+        this.openFileReadMode();
+        ArrayList<Student> students = new ArrayList<Student>();
+
+
+        try {
+            while (true) {
+                students.add((Student) input.readObject());
+            }
+        } catch (EOFException endOfFileException) {
+            System.err.println(""); //reading is finished
+        } catch (ClassNotFoundException classNotFoundException) {
+            System.err.println("Unable to create object.");
+        } catch (IOException ioException) {
+            System.err.println("Error during reading from file.");
+        }
+        this.closeFileReadMode();
+        
+        return students;
+
+    }
+
+    //directly overwritting the file
+    public void setStudents(ArrayList<Student> students) throws IOException{
+        this.openFileWriteMode();
+        for (Student student : students) {
+            output.writeObject(student);
+        }
+        this.closeFileWriteMode();
+    }
+    
+    //adding to the end of the file
+    public void addStudent(Student student){
+       ArrayList <Student> currentStudents = this.getStudents();
+       currentStudents.add(student);
+       
+       this.setStudents(currentStudents);
+    }
+
+    
+    //WRITEMODE
+    public void openFileWriteMode() {
+        try {
+            output = new ObjectOutputStream(new FileOutputStream(
+                    this.dbFilePath));
+        } catch (IOException ioException) {
+            System.err.println("Error opening file. WRITEMODE");
+        }
+    }
+
+    public void closeFileWriteMode() {
+        try {
+            if (output != null) {
+                output.close();
+            }
+        } catch (IOException ioException) {
+            System.err.println("Error closing file. WRITEMODE");
+            System.exit(1);
+        }
+    }
+
+    //READMODE
+    public void closeFileReadMode() {
+        try {
+            if (input != null) {
+                input.close();
+            }
+            System.exit(0);
+        } catch (IOException ioException) {
+            System.err.println("Error closing file. READMODE");
+            System.exit(1);
+        }
+    }
+
+    
+    public void openFileReadMode() {
+        try {
+            input = new ObjectInputStream(new FileInputStream(
+                    this.dbFilePath));
+        } catch (IOException ioException) {
+            System.err.println("Error opening file. READMODE");
+        }
+    }
 }
